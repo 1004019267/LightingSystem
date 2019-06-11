c1e9c74fc700f7ed0d9e7499a66a5dd6e4350e52
Unicorn <1004019267@qq.com>
2019-06-11 08:11:03 +0800

    LightingSystem

---
+++ Assets/ChangeShadowmaskMode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeShadowmaskMode : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (QualitySettings.shadowmaskMode==ShadowmaskMode.Shadowmask)
+        {
+            QualitySettings.shadowmaskMode = ShadowmaskMode.DistanceShadowmask;
+        }
+        else
+        {
+            QualitySettings.shadowmaskMode = ShadowmaskMode.Shadowmask;
+        }
+    }
+}
